e98eb4284e6954fa078d59e1111d125b7e5ba433
David <david.i.kaye@accenture.com>
2021-06-02 14:20:39 -0400

    AV-1696: API Usage Examples: Java: initial project setup

---
+++ java/src/test/java/com/agview/api/example/AppTest.java
@@ -0,0 +1,16 @@
+package com.agview.api.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+
+    @Test
+    public void addition() {
+        assertEquals(true, true);
+    }
+
+}
+
+
