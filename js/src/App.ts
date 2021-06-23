b24423fea91ea784e36fce75e93ffcc66d4c8663
David <david.i.kaye@accenture.com>
2021-06-23 12:41:50 -0400

    AV-1698: API Usage Examples: transform movement data

--- js/src/App.ts
+++ js/src/App.ts
@@ -1,17 +1,20 @@
 import { getMovementData, getMovementAddressesData } from "./MovementDbHandler"
 import { getAccessToken } from "./AccessTokenHandler"
 import Constants from './Constants'
+import { createMovements } from './MovementPostHandler'
 
 let movementDataPromise = getMovementData("../../db/movement.csv")
 let movementAddressesDataPromise = getMovementAddressesData("../../db/movement_addresses.csv")
 movementDataPromise.then((fileContent) => {
-    console.log(fileContent)
+    //console.log(fileContent)
 })
 movementAddressesDataPromise.then((fileContent) => {
-    console.log(fileContent)
+    //console.log(fileContent)
 })
 
 const env = process.env
 let accessTokenPromise = getAccessToken(Constants.NPB_BASE_URL, Constants.NPB_API_KEY, Constants.NPB_API_SECRET)
 
-accessTokenPromise.then((data) => console.log(data))
+//accessTokenPromise.then((data) => console.log(data))
+
+createMovements("../../db/movement.csv", "../../db/movement_addresses.csv")
--- js/src/MovementPostHandler.ts
+++ js/src/MovementPostHandler.ts
@@ -6,9 +6,18 @@ import Constants from './Constants'
 export function createMovements(movementFilePath: string, movementAddressesFilePath: string) {
     let movementPromise = getMovementData(movementFilePath)
     movementPromise.then((movementData) => {
-        let movementAddressesPromise = getMovementAddressesData(movementAddressesFilePath)
-        movementAddressesPromise.then((movementAddresses) => {
+        let movementsAddressesPromise = getMovementAddressesData(movementAddressesFilePath)
+        movementsAddressesPromise.then((movementsAddresses) => {
+            let movementAddressesByMovementId: Map<string, Map<string, any>> = new Map()
+            movementsAddresses.forEach(movementAddresses => {
+                movementAddressesByMovementId.set(movementAddresses.get('movement_id'), movementAddresses)
+            })
 
+            /*let requestBody: Array<{}> = []
+            movementAddressesByMovementId.forEach(movementAddresses => {
+                requestBody.push(Object.fromEntries(movementAddresses))
+            })
+            console.log(requestBody)*/
         })
     })
 }
