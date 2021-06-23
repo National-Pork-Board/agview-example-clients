55cc9e292e17eb2fd4ce347d95e10285ac67fb2d
David <david.i.kaye@accenture.com>
2021-06-23 13:30:48 -0400

    AV-1698: API Usage Examples: build create movements request body

--- js/src/MovementPostHandler.ts
+++ js/src/MovementPostHandler.ts
@@ -4,8 +4,8 @@ import { getAccessToken } from "./AccessTokenHandler"
 import Constants from './Constants'
 
 export function createMovements(movementFilePath: string, movementAddressesFilePath: string) {
-    let movementPromise = getMovementData(movementFilePath)
-    movementPromise.then((movementData) => {
+    let movementsPromise = getMovementData(movementFilePath)
+    movementsPromise.then((movements) => {
         let movementsAddressesPromise = getMovementAddressesData(movementAddressesFilePath)
         movementsAddressesPromise.then((movementsAddresses) => {
             let movementAddressesByMovementId: Map<string, Map<string, any>> = new Map()
@@ -13,11 +13,53 @@ export function createMovements(movementFilePath: string, movementAddressesFileP
                 movementAddressesByMovementId.set(movementAddresses.get('movement_id'), movementAddresses)
             })
 
-            /*let requestBody: Array<{}> = []
-            movementAddressesByMovementId.forEach(movementAddresses => {
-                requestBody.push(Object.fromEntries(movementAddresses))
+            //Create request body
+            let requestBody: Array<{}> = []
+            movements.forEach(movement => {
+                let source = getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source')
+                var sourceLatitude = getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source_latitude')
+                var target = getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination')
+                var targetLatitude = getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination_latitude')
+
+                let requestBodyEntry: any = {}
+                requestBodyEntry['species'] = movement.get('species')
+                requestBodyEntry["numberInShipment"] = movement.get('number_in_shipment')
+                requestBodyEntry["movementType"] = movement.get('movement_type')
+                requestBodyEntry["movementDatetime"] = movement.get('movement_datetime')
+
+                requestBodyEntry["source"] = source;
+                requestBodyEntry["sourceLatitude"] = sourceLatitude;
+                requestBodyEntry["sourceLongitude"] = getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source_longitude')
+                if (!source && !sourceLatitude) {
+                    requestBodyEntry["sourceAddress"] =
+                    {
+                        "streetAddress": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source_street_address'),
+                        "city": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source_city'),
+                        "state": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source_state'),
+                        "zip": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('source_zip')
+                    }
+                }
+
+                requestBodyEntry["destination"] = target;
+                requestBodyEntry["destinationLatitude"] = targetLatitude;
+                requestBodyEntry["destinationLongitude"] = getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination_longitude')
+                if (!target && !targetLatitude) {
+                    requestBodyEntry["destinationAddress"] =
+                    {
+                        "streetAddress": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination_street_address'),
+                        "city": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination_city'),
+                        "state": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination_state'),
+                        "zip": getCorrespondingAddressesRecord(movementAddressesByMovementId, movement).get('destination_zip')
+                    }
+                }
+
+                requestBody.push(requestBodyEntry)
             })
-            console.log(requestBody)*/
         })
     })
+}
+
+function getCorrespondingAddressesRecord(movementAddressesByMovementId: Map<string, Map<string, any>>, movement: Map<string, any>): Map<string, any> {
+
+    return movementAddressesByMovementId.get(movement.get('movement_id'))!
 }
