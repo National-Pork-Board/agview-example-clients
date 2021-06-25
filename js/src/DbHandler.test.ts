0097a1924eb5257c09a5c85cf328b7267b52f940
David <david.i.kaye@accenture.com>
2021-06-25 14:30:26 -0400

    AV-1698: API Usage Examples: JS: simplify run & test commands

--- js/src/DbHandler.test.ts
+++ js/src/DbHandler.test.ts
@@ -3,11 +3,15 @@ import { getData } from './DbHandler'
 const PREM_FILE_PATH = '../../db/prem.csv'
 
 describe('DbHandler', () => {
-    it('gets premise data', async () => {
+    it('gets data', async () => {
         let actual = await getData(PREM_FILE_PATH)
 
         expect(actual.length).toEqual(2)
         expect(actual[0].get('usda_pin')).not.toBeNull()
         expect(actual[1].get('usda_pin')).not.toBeNull()
+    });
+
+    it('gets column names', async () => {
+        let actual = await getData(PREM_FILE_PATH)
     });
 })
